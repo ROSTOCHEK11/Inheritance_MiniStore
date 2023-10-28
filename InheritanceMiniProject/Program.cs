5f22152418e7f83a75977ab133cd87f44b6b6695
ROSTOCHEK11 <rostzhol@gmail.com>
2023-10-28 14:06:51 +0200

    1) Updated addition to List 2) Updated user interface interaction ( Buy section )

--- InheritanceMiniProject/Program.cs
+++ InheritanceMiniProject/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
@@ -14,49 +15,99 @@ namespace InheritanceMiniProject
     {
         static void Main(string[] args)
         {
-
             
-            List<IRentable> rentables = new List<IRentable>();
-            List<IPurchasable> purchasables = new List<IPurchasable>();
-
-            var manga = new MangaModel { ProductName = "Chainsaw Man Manga", NumberOfPages = 123, Quantity = 2, Price = 10 };
-            var manga2 = new MangaModel { ProductName = "Demon Slayer Manga", NumberOfPages = 67, Quantity = 1, Price = 8 };
+            var manga = new MangaModel { ProductName = "Chainsaw Man M.", NumberOfPages = 123, Quantity = 2, Price = 10 };
+            var manga2 = new MangaModel { ProductName = "Demon Slayer M.", NumberOfPages = 67, Quantity = 1, Price = 8 };
 
             var figure = new FigureModel { ProductName = "Naruto figure", Height = "15 cm", Quantity = 2, Price = 15 };
             var figure2 = new FigureModel { ProductName = "Luffy figure", Height = "35 cm", Quantity = 2, Price = 40 };
 
-            var poster = new PosterModel { ProductName = "One Piece poster", Size = "50x30 cm", Quantity = 3, Price = 6 };
+            var poster = new PosterModel { ProductName = "OnePiece poster", Size = "50x30 cm", Quantity = 3, Price = 6 };
             var poster2 = new PosterModel { ProductName = "SAO poster", Size = "20x15 cm", Quantity = 2, Price = 6 };
             
             var costume = new CostumeModel { ProductName = "Pirate costume", Material = "100% Cotton", Quantity = 1, Price = 25, RentPrice = 9 };
-            var costume2 = new CostumeModel { ProductName = "Goku costume", Material = "60% Cotton, 40% Poliester", Quantity = 2, Price = 23, RentPrice = 9 };
+            var costume2 = new CostumeModel { ProductName = "Goku costume", Material = "100% Silk", Quantity = 2, Price = 23, RentPrice = 9 };
 
             var animator = new AnimatorModel { ProductName = "Animator - Pikachu", Quantity = 1, RentPrice = 50 };
             var animator2 = new AnimatorModel { ProductName = "Animator - Naruto", Quantity = 1, RentPrice = 50 };
 
 
-            rentables.Add(costume);
-            rentables.Add(costume2);
-            rentables.Add(animator);
-            rentables.Add(animator2);
+            var items = new List<IInventoryModel> { manga, manga2, figure, figure2, poster, poster2, costume, costume2, animator, animator2 };
+            List<IRentable> rentables = new List<IRentable>();
+            List<IPurchasable> purchasables = new List<IPurchasable>();
 
-            purchasables.Add(manga);
-            purchasables.Add(manga2);
-            purchasables.Add(figure);
-            purchasables.Add(figure2);
-            purchasables.Add(poster); 
-            purchasables.Add(poster2);
-            purchasables.Add(costume);
-            purchasables.Add(costume2);
 
-            foreach (var item in purchasables)
+            foreach (var item in items)
             {
-                item.PrintItem();
+                if (item is IPurchasable)
+                {
+                    purchasables.Add(item as IPurchasable);
+                }
+                else if (item is IRentable)
+                {
+                    rentables.Add(item as IRentable);
+                }
             }
 
 
+            Write("Hello to my mini Anime Store!\nDo you want to BUY or RENT something ? ( buy / rent ): ");
+            string rentalDecision = ReadLine();
+
+            if(rentalDecision.ToLower() == "buy")
+            {
+
+                WriteLine("\nHere's some Items to buy :\n\n");
+
+                
+                string continueAnswer;
+                List<IPurchasable> purchases = new List<IPurchasable>();
+                do
+                {
+                    int counter = 1;
+                    foreach (var item in purchasables)
+                    {
+                        Write($"{counter})\t");
+                        item.PrintItem();
+                        WriteLine();
+                        counter++;
+
+                    }
+
+                    Write("Which one do you want ? ( Write the number ) : ");
+                    int productChoice = int.Parse(ReadLine());
+
+                    IPurchasable productChoicePurchase = purchasables[productChoice - 1];
+
+                    if(productChoicePurchase.Quantity > 0)
+                    {
+                        productChoicePurchase.Purchase();
+                        purchases.Add(productChoicePurchase);
+                    }
+                    else
+                    {
+                        WriteLine("This product is out of stock");
+                    }
+                    
+
+                    Write("\nDo you want something else ? ( yes / no ) : "); 
+                    continueAnswer = ReadLine();
+
+                    Clear();
+                    
+
+                } while (continueAnswer.ToLower() != "no");
+
+                WriteLine("Products that you bought : \n");
+                foreach(var item in purchases)
+                {
+                    WriteLine(item.ProductName);
+                }
+                WriteLine("\nThank's for visiting us !");
+            }
+
 
             ReadLine();
+            
 
         }
 
@@ -121,9 +172,10 @@ namespace InheritanceMiniProject
         public void Purchase()
         {
             Quantity -= 1;  
-            WriteLine("This Manga has been Purchased ");
+            WriteLine($"{ProductName} has been Purchased ");
 
         }
+
 
     }
 
@@ -134,13 +186,13 @@ namespace InheritanceMiniProject
 
         void IPurchasable.PrintItem()
         {
-            WriteLine($"{ProductName}\t|\tHeight: {Height}\t|\tPrice: {Price}\t|\tQuantity: {Quantity}");
+            WriteLine($"{ProductName}\t|\tHeight: {Height}\t\t|\tPrice: {Price}\t|\tQuantity: {Quantity}");
         }
 
         public void Purchase()
         {
             Quantity -= 1;
-            WriteLine("This Figure has been Purchased ");
+            WriteLine($"{ProductName} has been Purchased ");
 
         }
     }
@@ -151,12 +203,12 @@ namespace InheritanceMiniProject
         public decimal Price { get; set; }
         void IPurchasable.PrintItem()
         {
-            WriteLine($"{ProductName}\t|\tSize: {Size}\t|\tPrice: {Price}\t|\tQuantity: {Quantity}");
+            WriteLine($"{ProductName}\t|\tSize: {Size}\t\t|\tPrice: {Price}\t|\tQuantity: {Quantity}");
         }
         public void Purchase()
         {
             Quantity -= 1;
-            WriteLine("This Poster has been Purchased ");
+            WriteLine($"{ProductName} has been Purchased ");
 
         }
     }
@@ -174,26 +226,26 @@ namespace InheritanceMiniProject
 
         void IRentable.PrintItem()
         {
-            WriteLine($"{ProductName}\t|\tMaterial: {Material}\t|\tRent price: {RentPrice}\t|\tQuantity: {Quantity}");
+            WriteLine($"{ProductName}\t\t|\tMaterial: {Material}\t|\tRent price: {RentPrice}\t|\tQuantity: {Quantity}");
         }
 
         public void Purchase()
         {
             Quantity -= 1;
-            WriteLine("This Costume has been Purchased ");
+            WriteLine($"{ProductName} has been Purchased ");
 
         }
 
         public void Rent()
         {
             Quantity -= 1;
-            WriteLine("This Costume has been Rented ");
+            WriteLine($"{ProductName} has been Rented ");
         }
 
         public void ReturnRent()
         {
             Quantity += 1;
-            WriteLine("This Costume has been Returned ");
+            WriteLine($"{ProductName} has been Returned ");
         }
     }
 
@@ -203,19 +255,19 @@ namespace InheritanceMiniProject
 
         void IRentable.PrintItem()
         {
-            WriteLine($"{ProductName}\t|\tRent price: {RentPrice}\t|\tQuantity: {Quantity}");
+            WriteLine($"{ProductName}\t|\tRent price: {RentPrice}\t\t|\tQuantity: {Quantity}\t|");
         }
 
         public void Rent()
         {
             Quantity -= 1;
-            WriteLine("This Animator has been Rented ");
+            WriteLine($"{ProductName} has been Rented ");
         }
 
         public void ReturnRent()
         {
             Quantity += 1;
-            WriteLine("This Animator has been Returned ");
+            WriteLine($"{ProductName} has been Returned ");
         }
     }
 
